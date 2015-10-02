59100fe895e14d6f133970e7243f3e2fcb144972
carmennegreanu <Carmen.Negreanu@endava.com>
2015-10-02 10:51:24 +0300

    Added TrainingController for the subscription functionality.

---
+++ Source/GB.tnLabs.Web/Controllers/TrainingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GB.tnLabs.Web.Controllers
+{
+    public class TrainingController : Controller
+    {
+        // GET: Training
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
